aeb6b93eafdda1959826b32691b838cf0952182b
chriswfong <120702148+chriswfong@users.noreply.github.com>
2022-12-16 21:51:59 -0800

    Structured Project
    Finished adding card list components and added assets

--- movieList/src/index.js
+++ movieList/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './containers/List';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-	return <h1>movieList</h1>;
+	return <List />;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
